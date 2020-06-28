ec230423ea1dbd56406291896c8790fb57149789
WaitingChen <WaitingChen>
2020-06-28 15:49:10 +0800

    second commit

---
+++ src/components/views/First.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="first-app">
+        {{msg}}
+        <confirm text="注册" @message="getMessage"></confirm>
+        <p><router-link to="/second">去第二个页面</router-link></p>
+    </div>
+</template>
+
+<script>
+import Confirm from "../sub/Confirm";
+export default {
+    name:"First",
+    components:{
+        Confirm
+    },
+    data () {
+        return {
+            msg:"welcome to FirstApp"
+        }
+    },
+    methods: {
+        getMessage(val) {
+            console.log(val);
+        }
+    }
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/views/Second.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="second-app">
+    {{msg}}
+    <comfirm text="注册" @message="getMessage"></comfirm>
+    <p class="link">
+      <router-link to="/first">去第一个页面</router-link>
+    </p>
+  </div>
+</template>
+<script>
+import Comfirm from "@/components/sub/Confirm";
+export default {
+  name: "second-app",
+  components: {
+    Comfirm
+  },
+  data() {
+    return {
+      msg: "wecome to second-app"
+    };
+  },
+  methods: {
+    getMessage(val) {
+      console.log(val);
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.second-app {
+  font-size: 30px;
+  .link {
+    a {
+      color: #f9f;
+    }
+  }
+}
+</style>
